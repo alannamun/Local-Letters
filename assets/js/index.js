0304dd2199046d558f7ab6cba87b8bc0edb3d2f5
alannamun <alannamunro@Alexs-iMac-27.local>
2014-06-18 01:03:55 -0700

    Add the assets

---
+++ assets/js/index.js
@@ -0,0 +1,79 @@
+/**
+ * Main JS file for Casper behaviours
+ */
+
+ /* Add typekit to this theme */
+  (function(d) {
+    var config = {
+      kitId: 'gqa3kdf',
+      scriptTimeout: 3000
+    },
+    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+  })(document);
+
+/*globals jQuery, document */
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+
+        $(".post-content").fitVids();
+
+        function casperFullImg() {
+            $("img").each( function() {
+                var contentWidth = $(".post-content").outerWidth(); // Width of the content
+                var imageWidth = $(this)[0].naturalWidth; // Original image resolution
+
+                if (imageWidth >= contentWidth) {
+                    $(this).addClass('full-img');
+                } else {
+                    $(this).removeClass('full-img');
+                }
+            });
+        };
+
+        casperFullImg();
+        $(window).smartresize(casperFullImg);
+
+    });
+
+}(jQuery));
+
+(function($,sr){
+
+  /* A text swapper */
+  var swap = document.querySelectorAll("swap")[0];
+swap.addEventListener('click', function() {
+  if (swap.getAttribute("data-text-swap") == swap.innerHTML) {
+    swap.innerHTML = swap.getAttribute("data-text-original");
+  } else {
+    swap.setAttribute("data-text-original", swap.innerHTML);
+    swap.innerHTML = swap.getAttribute("data-text-swap");
+  }
+}, false);
+
+  // debouncing function from John Hann
+  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+  var debounce = function (func, threshold, execAsap) {
+      var timeout;
+
+      return function debounced () {
+          var obj = this, args = arguments;
+          function delayed () {
+              if (!execAsap)
+                  func.apply(obj, args);
+              timeout = null;
+          };
+
+          if (timeout)
+              clearTimeout(timeout);
+          else if (execAsap)
+              func.apply(obj, args);
+
+          timeout = setTimeout(delayed, threshold || 100);
+      };
+  }
+  // smartresize 
+  jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+
+})(jQuery,'smartresize');
